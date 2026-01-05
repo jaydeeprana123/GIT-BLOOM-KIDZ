0233e1470001512e693f7a2114bb3259a6faf71b
jaydeep_rana <jaideep1210@gmail.com>
2026-01-05 18:38:13 +0530

    HTTP TO https

--- lib/Networks/api_endpoint.dart
+++ lib/Networks/api_endpoint.dart
@@ -1,6 +1,6 @@
 // live url
 var urlBase =
-    'http://api.bloomkidz.net/api'; // change ( 10-07-2024 ) ( Live url)
+    'https://api.bloomkidz.net/api'; // change ( 10-07-2024 ) ( Live url)
 
 //Authentication APIs
 const urlLogin = '/login';
